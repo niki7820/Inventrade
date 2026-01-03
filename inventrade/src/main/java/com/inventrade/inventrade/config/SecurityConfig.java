44c1e9a4724bb3b11eb9d96b8942c14fe608449a
Pranay7020 <pranayshinde872@gmail.com>
2026-01-03 14:16:54 +0530

    added health check

--- inventrade/src/main/java/com/inventrade/inventrade/config/SecurityConfig.java
+++ inventrade/src/main/java/com/inventrade/inventrade/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.inventrade.inventrade.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,18 +12,35 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/css/**", "/js/**").permitAll() // static resources
+                        .requestMatchers("/health").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
-                        .defaultSuccessUrl("/dashboard", true) // 🔑 redirect to product after login
+                        .defaultSuccessUrl("/dashboard", true)
                 )
                 .logout(logout -> logout.permitAll());
 
         return http.build();
     }
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/health", "/css/**", "/js/**").permitAll()
+//                        .anyRequest().authenticated()
+//                )
+//                .formLogin(form -> form
+//                        .defaultSuccessUrl("/dashboard", true)
+//                        .permitAll()
+//                )
+//                .logout(logout -> logout.permitAll());
+//
+//        return http.build();
+//    }
+
 }
--- inventrade/src/main/java/com/inventrade/inventrade/controller/ITController.java
+++ inventrade/src/main/java/com/inventrade/inventrade/controller/ITController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import java.security.Principal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Controller
@@ -22,10 +23,16 @@ public class ITController {
     @Autowired
     private ProductService productService;
 
-    @GetMapping("/login")
-    public String login() {
-        return "login";
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/login";
     }
+
+
+//    @GetMapping("/login")
+//    public String login() {
+//        return "login";
+//    }
 
     @GetMapping("/dashboard")
     public String dashboard(Model model, Principal principal) {
